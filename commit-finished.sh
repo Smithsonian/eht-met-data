1aab9865c9a0b5390df369a6e8d7b4b1c8782912
Greg Lindahl <lindahl@pbm.com>
2020-10-21 22:38:35 -0700

    autocommit

--- commit-finished.sh
+++ commit-finished.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd ~/eht-met-data
-find . -type f | xargs wc -l | grep  ' 210 ' | awk '{print $2}' | xargs git add
+find . -type f | grep /202 | xargs wc -l | grep  ' 210 ' | awk '{print $2}' | xargs git add
 git commit -m autocommit .
 git push
