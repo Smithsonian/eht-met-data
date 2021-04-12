506b334c97027434b23ec2d81cbb86d7501733bf
Greg Lindahl <lindahl@pbm.com>
2021-04-12 09:50:06 -0700

    speed up commit by only trying 2021 and later

--- commit-finished.sh
+++ commit-finished.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-find . -type f | grep /202 | xargs wc -l | grep  ' 210 ' | awk '{print $2}' | xargs git add
+# the find is limited to 2021 and later, to make this run faster
+find . -type f | grep /202[123456789] | xargs wc -l | grep  ' 210 ' | awk '{print $2}' | xargs git add
 git commit -m autocommit .
 git push
