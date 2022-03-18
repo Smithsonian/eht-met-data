aa0d23b17bcc5e5fd81a86125933f31d80b39970
Greg Lindahl <lindahl@pbm.com>
2022-03-18 08:06:26 -0700

    autocommit

--- commit-finished.sh
+++ commit-finished.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# the find is limited to 2021 and later, to make this run faster
-find . -type f | grep /202[123456789] | xargs wc -l | grep  ' 210 ' | awk '{print $2}' | xargs git add
+git pull
+# the find is limited to 2022 and later, to make this run faster
+find . -type f | grep /202[23456789] | xargs wc -l | grep  ' 210 ' | awk '{print $2}' | xargs git add
 git commit -m autocommit .
 git push
