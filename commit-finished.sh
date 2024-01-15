f9086c7628cfec5ede8797777a0a98dc31ebcfce
Greg Lindahl <lindahl@pbm.com>
2024-01-15 12:45:09 -0800

    2024 now

--- commit-finished.sh
+++ commit-finished.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git pull
-# the find is limited to 2023 and later, to make this run faster
-find . -type f | grep /202[3456789] | xargs wc -l | grep  ' 210 ' | awk '{print $2}' | xargs git add
+# the find is limited to 2024 and later, to make this run faster
+find . -type f | grep /202[456789] | xargs wc -l | grep  ' 210 ' | awk '{print $2}' | xargs git add
 git commit -m autocommit .
 git push
